ada6ccac51514292d50d24ec1f974b0936fdefb7
Dilan Jayasinghe <dilan.pramodya97@gmail.com>
2021-01-29 23:08:00 +0530

    Initial Commit

---
+++ PLEX-UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PLEX-UI
+//
+//  Created by Dilan Pramodya on 2021-01-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
